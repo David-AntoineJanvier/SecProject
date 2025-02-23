0cad7f8f3246c9df313cde21f115b957a01b4fac
Wazurim <wazajust4you51@gmail.com>
2025-02-23 17:25:13 -0500

    oupsy3 - maudit que la vm est mal faites

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include "user_data.h"
+#include "auth.h"
 
 int main()
 {
     printf("Bienvenu dans le super systeme de collection des donnée ultra securitaire !\n\n");
     printf("Veuillez vous authentifier pour acceder au systeme.\n");
-    
+    AuthentificationParameters_t test = auth_login();
+    printf("Vous essayer de vous connecte en tant que %s.\n", test.username);
+
+
 
     /*
     AuthentificationParameters_t heyKracko = {.username = "kaobilin", .password = "before\0"};
